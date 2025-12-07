90647cdca683f8708300eeb2a6b942a5e12af900
Emyerson4444 <ezra_myerson@brown.edu>
2025-12-07 17:28:55 +0000

    matlab

---
+++ bpnet/dataset.py
@@ -0,0 +1,31 @@
+import os
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+    
+    
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+folder_id = "1QZ-Z-C9MoLk1S9MovF3Tt6NjAEGbSJI3"
+
+file_list = drive.ListFile({'q': f"'{folder_id}' in parents"}).GetList()
+save_dir_vital = "Segment_Files/PulseDB_Vital"
+os.makedirs(save_dir, exist_ok=True)
+
+for file in file_list:
+    filename = file['title']
+    filepath = os.path.join(save_dir_vital, filename)
+    file.GetContentFile(filepath)
+
+folder_id = "1-X6832Gs2CViMofq_jJLKgzXNX-Sx8Oq"
+
+file_list = drive.ListFile({'q': f"'{folder_id}' in parents"}).GetList()
+save_dir_MIMIC = "Segment_Files/PulseDB_MIMIC"
+os.makedirs(save_dir, exist_ok=True)
+
+for file in file_list:
+    filename = file['title']
+    filepath = os.path.join(save_dir_MIMIC, filename)
+    file.GetContentFile(filepath)
+
